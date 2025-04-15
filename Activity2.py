f41ae7aae5d95218c38b3164291a47542835a0d9
Vivienne126 <vivienne.singh@cns.ac.in>
2025-04-15 17:26:42 +0530

    New1

---
+++ Activity2.py
@@ -0,0 +1,7 @@
+class vehicals:
+    def __init__(self, max_speed,mileage):
+        self.max_speed=max_speed
+        self.mileage=mileage
+Modelx=vehicals(240,18)
+print("The maximum speed is" , Modelx.max_speed)
+print("The milage is" , Modelx.mileage)
